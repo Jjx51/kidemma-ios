3500ced49fe17d34050c2d3b14e1dac4ddf50345
Alfredo Hernández <leonardo.alfredo.hernandez.reyes@gmail.com>
2026-02-10 18:27:37 -0600

    Crete the Admin TabVar and the ToolBar

---
+++ com.kidemma/Modules/TabBar/Presentation/Components/TopBar.swift
@@ -0,0 +1,36 @@
+//
+//  TopBar.swift
+//  com.kidemma
+//
+//  Created by leonardo.hernandez on 10/02/26.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    //TODO: Use the real username
+    var userName: String = "Alfredo"
+    
+    var body: some View {
+        HStack {
+            Text(String(userName.prefix(1)))
+                .padding(18)
+                .background(Color("BackgroundCard"))
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color("IconColor"), lineWidth: 2)
+                )
+            Image("ic_kidemma_topbar")
+            Spacer()
+            Image("ic_notifications")
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 4)
+        .background(.white)
+    }
+}
+
+#Preview {
+    TopBar()
+}
